0beb4f2c13830d9238d46994f56acde0018d1d71
marceldonin <83168668+marceldonin@users.noreply.github.com>
2021-09-14 22:57:39 -0300

    Atualização e inclusão do JS

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/*
+Theme Name: Avaliação Dissertativa 02
+Class: T72021
+Subject: Linguagem HTML, CSS e JavaScript (56h)
+Theme URI: https://marceldonin.github.io/Projeto-HAM-BUG-WebDesign/
+Author: Marcel Donin, Andiara Cossetin e Hilberto Carlos Schaurich
+Author URI: https://github.com/marceldonin
+Description: Projeto HAM BUG WebDesign
+Data: 08/09/21  
+Version: 1.0
+
+--------------------------------------------
+ESTRUTURA BASE
+--------------------------------------------*/
+
+
+/***** DECLARAÇÃO DE VARIAVEIS*/
+/***** ESTILOS GERAIS*/
+/***** BODY*/
+/***** HEADER*/
+/***** FOOTER*/
+/***** PÁGINA INICIAL*/
+/***** EXTRAS*/
+/***** @MEDIA*/
